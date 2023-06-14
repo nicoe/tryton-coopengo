7f8b003b16b125af8e735ae678dca346202e2a3b
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:07:17 +0200

    [sao] Add missing semi colon [CUSTOM]

--- sao/src/model.js
+++ sao/src/model.js
@@ -1567,7 +1567,7 @@
         set_default: function(record, value) {
             var promise = this.set(record, value);
             record.modified_fields[this.name] = true;
-            return promise
+            return promise;
         },
         set_on_change: function(record, value) {
             this.set(record, value);
