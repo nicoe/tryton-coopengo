980c6b88fd17ab27f4cffa1e40af3212f9696031
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:04:00 +0200

    [modules/account_statement] Allow move line customisation [CUSTOM]

--- modules/account_statement/statement.py
+++ modules/account_statement/statement.py
@@ -318,7 +318,7 @@ class Statement(Workflow, ModelSQL, ModelView):
                             except AttributeError:
                                 pass
                         new_line.amount = line.amount + amount_to_pay
-                        new_line.invoice = None
+                        new_line.reset_remaining_line(line)
                         line_offset += 1
                         lines.insert(index + line_offset, new_line)
                         invoice_id2amount_to_pay[line.invoice.id] = 0
@@ -1029,6 +1029,9 @@ class Line(origin_mixin(_states), sequence_ordered(), ModelSQL, ModelView):
             second_currency=second_currency,
             amount_second_currency=amount_second_currency,
             )
+
+    def reset_remaining_line(self, from_line):
+        self.invoice = None
 
 
 del _states
