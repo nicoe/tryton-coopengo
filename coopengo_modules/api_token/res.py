de39635fd667b03a37c77bed565df5b7878263c9
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:03:03 +0200

    [coopengo_modules/api_token] api_token: Ensure token is unique and allow to generate new one (#3)

---
+++ coopengo_modules/api_token/res.py
@@ -0,0 +1,15 @@
+# This file is part of Coog. The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+from trytond.pool import PoolMeta
+
+from trytond.model import fields
+
+__all__ = [
+    'User',
+    ]
+
+
+class User(metaclass=PoolMeta):
+    __name__ = 'res.user'
+
+    tokens = fields.One2Many('api.token', 'user', 'Tokens')
