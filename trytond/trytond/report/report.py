2afa68e02f6676d88cf3f5cad866d01224a29846
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:03:36 +0200

    [trytond] Add support for post 2013 docx and xlsx in Report [CUSTOM]
    Fix #8

--- trytond/trytond/report/report.py
+++ trytond/trytond/report/report.py
@@ -75,6 +75,7 @@ FORMAT2EXT = {
     'doc95': 'doc',
     'docbook': 'xml',
     'docx7': 'docx',
+    'docx': 'docx',
     'ooxml': 'xml',
     'latex': 'ltx',
     'sdc4': 'sdc',
@@ -89,6 +90,7 @@ FORMAT2EXT = {
     'xhtml': 'html',
     'xls5': 'xls',
     'xls95': 'xls',
+    'xlsx': 'xlsx',
     }
 
 TIMEDELTA_DEFAULT_CONVERTER = {
