e0d4056f967871c84ce71228f7ac15d631ff06f9
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:06:19 +0200

    [sao] Get dist out of source control [CUSTOM]
    Fix #8180

--- sao/Gruntfile.js
+++ sao/Gruntfile.js
@@ -179,5 +179,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.task.run(['concat', 'jshint', 'less:dev', 'qunit']);
     });
+  grunt.registerTask('copy', 'Copy resources to dist', function() {
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.task.run(['copy']);
+  });
 
 };
