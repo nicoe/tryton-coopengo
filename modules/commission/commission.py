1badeaec70dafd06b4c6a9f4fb6401dc36fe1351
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:04:11 +0200

    [modules/commission] Make modular commissions to delete / cancel computation [CUSTOM]
    Fix#3761
    https://redmine.coopengo.com/issues/3761
    http://rietveld.coopengo.com/21580001
    review http://rietveld.coopengo.com/22410001

--- modules/commission/commission.py
+++ modules/commission/commission.py
@@ -464,6 +464,17 @@ class Commission(ModelSQL, ModelView):
         Invoice.update_taxes(invoices)
         cls.save(to_save)
 
+    @classmethod
+    def cancel(cls, commissions):
+        cancel_commissions = cls.copy(commissions)
+        for commission in cancel_commissions:
+            commission.update_cancel_copy()
+        cls.save(cancel_commissions)
+        return cancel_commissions
+
+    def update_cancel_copy(self):
+        self.amount *= -1
+
     def _group_to_invoice_key(self):
         direction = {
             'in': 'out',
