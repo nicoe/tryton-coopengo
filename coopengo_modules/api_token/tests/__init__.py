792abb537dda626f7d87c127fff4ca70f3afda0e
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:04:16 +0200

    [coopengo_modules/api_token] api_token: Update tests to Tryton 6.4 (#7)
    Fix #00000

--- coopengo_modules/api_token/tests/__init__.py
+++ coopengo_modules/api_token/tests/__init__.py
@@ -1,9 +1,2 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-
-try:
-    from trytond.modules.api_token.tests.test_api_token import suite
-except ImportError:
-    from .test_api_token import suite
-
-__all__ = ['suite']
--- coopengo_modules/api_token/tests/test_api_token.py
+++ coopengo_modules/api_token/tests/test_api_token.py
@@ -1,8 +1,5 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-import unittest
-
-import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 
 
@@ -11,8 +8,4 @@ class APITokenTestCase(ModuleTestCase):
     module = 'api_token'
 
 
-def suite():
-    suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            APITokenTestCase))
-    return suite
+del ModuleTestCase
