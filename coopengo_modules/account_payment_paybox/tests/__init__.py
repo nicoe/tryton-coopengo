c874d27dc6fb5c1fde65a6cc0b47021b0cfa0812
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:04:17 +0200

    [coopengo_modules/account_payment_paybox] account_payment_paybox: Update tests to Tryton 6.4 (#17)
    Fix #00000

--- coopengo_modules/account_payment_paybox/tests/__init__.py
+++ coopengo_modules/account_payment_paybox/tests/__init__.py
@@ -1,9 +1,2 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-
-try:
-    from trytond.modules.account_payment_paybox.tests.test_account import suite
-except ImportError:
-    from .test_account import suite
-
-__all__ = ['suite']
--- coopengo_modules/account_payment_paybox/tests/test_account.py
+++ coopengo_modules/account_payment_paybox/tests/test_account.py
@@ -1,12 +1,10 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-import unittest
 try:
     import phonenumbers
 except ImportError:
     phonenumbers = None
 
-import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 
 
@@ -15,8 +13,4 @@ class AccountTestCase(ModuleTestCase):
     module = 'account_payment_paybox'
 
 
-def suite():
-    suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            AccountTestCase))
-    return suite
+del ModuleTestCase
