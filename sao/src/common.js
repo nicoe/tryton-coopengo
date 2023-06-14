c42c153d61227ca25bfa30435c1a77e145f34da3
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:07:09 +0200

    [sao] Fix recursive context and wrong handling of tab_domain in search_count (#113) [CUSTOM]
    Fix #19009

--- sao/src/common.js
+++ sao/src/common.js
@@ -4239,4 +4239,14 @@
         return el.html();
     };
 
+    Sao.common.clone = function(obj) {
+        var copy = obj.constructor();
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) {
+                copy[attr] = obj[attr];
+            }
+        }
+        return copy;
+    };
+
 }());
