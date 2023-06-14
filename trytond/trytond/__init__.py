ba651317acee77bb6502558a39ba9e070398fa50
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:07:33 +0200

    [trytond] Set the fallback application name when connecting to postgres [PREVIEW] (#224)
    PCOOG-15

--- trytond/trytond/__init__.py
+++ trytond/trytond/__init__.py
@@ -1,6 +1,7 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 import os
+import sys
 import time
 import warnings
 from email import charset
