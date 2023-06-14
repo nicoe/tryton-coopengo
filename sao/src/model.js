fc9aaa0342bfbbd07c698b6da8dc7e634c94ecb4
Jean Cavallo <jean.cavallo@hotmail.fr>
2023-06-14 17:07:17 +0200

    [sao] Return rec_name promise when available (#129) [CUSTOM]
    Fix #21445

--- sao/src/model.js
+++ sao/src/model.js
@@ -1565,8 +1565,9 @@
             return this.get(record);
         },
         set_default: function(record, value) {
-            this.set(record, value);
+            var promise = this.set(record, value);
             record.modified_fields[this.name] = true;
+            return promise
         },
         set_on_change: function(record, value) {
             this.set(record, value);
@@ -2095,6 +2096,7 @@
             return rec_name;
         },
         set: function(record, value) {
+            var promise;
             var rec_name = (
                 record._values[this.name + '.'] || {}).rec_name || '';
             if (!rec_name && (value >= 0) && (value !== null)) {
@@ -2108,6 +2110,7 @@
             Sao.setdefault(
                 record._values, this.name + '.', {}).rec_name = rec_name;
             record._values[this.name] = value;
+            return promise;
         },
         set_client: function(record, value, force_change) {
             var rec_name;
